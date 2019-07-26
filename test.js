504baba62c94a6cd3c7e5f5315eb1c9ae570037c
Danila <snetkov.da@students.dvfu.ru>
2019-07-26 21:39:33 +1000

    Update deck and test to new format + now u can run all test at once

--- test.js
+++ test.js
@@ -1,142 +1,111 @@
+function TestAll(){
+    return("line: " + TestLine() + "\n" +
+        "Acsess: " + TestAcsess() + "\n" +
+        "Pipe: " + TestPipe() + "\n" +
+        "dead end check: " + TestReacheable() + "\n" +
+        "gap test: " + TestGap()+ "\n");
+}
+
 function TestLine () {
 
-	for (let i = 0;i<7;i++){
-		let card = new PathCard("no", "no", "yes","yes");
-		let result = field.availableSpaces(card);
-		let rightest = -1;
-		for(let j = 0; j< result.length; j++){
-			if (result[j][0] >= rightest){
-				rightest = result[j][0];
-			}
-		}
-		field.place(card,rightest, 0);
-	}
+    let testField = new Field();
+    testField.grid[0][0] = 0;
+    let checkString = "";
+    for (let i = 0;i<7;i++){
+        let card = 21;
+        let result = testField.availableSpaces(card);
+        let rightest = -1;
+        for(let j = 0; j< result.length; j++){
+            if (result[j][0] >= rightest){
+                rightest = result[j][0];
+            }
+        }
+        testField.place(card,rightest, 0);
+        checkString += rightest;
+    }
+    return (checkString === "1234567");
 }
 
 function TestAcsess(){
 
-	let card1 = new PathCard("yes", "no", "no", "no");
-	let card2 = new PathCard("no", "yes", "no", "no");
-	let card3 = new PathCard("no", "no", "yes", "no");
-	let card4= new PathCard("no", "no", "no", "yes");
-	field.place(card1, 0, -1);
-	field.place(card2, 0, 1);
-	field.place(card3, -1, 0);
-	field.place(card4, 1, 0);	
-	let result = field.availableSpaces(card1);
-	if (result.length === 0){
-		console.log("true");
-	}
-	else{
-		console.log("false");
-	}
+    let testField = new Field();
+    let card1 = 41;
+    let card2 = 42;
+    let card3 = 20;
+    let card4= 19;
+    testField.place(card1, 0, -1);
+    testField.place(card2, 0, 1);
+    testField.place(card3, -1, 0);
+    testField.place(card4, 1, 0);
+    let result;
+    let card;
+    let returnVal = true;
+    for (let i = 0; i< 40 ; i++) {
+        card = i;
+        result  = testField.availableSpaces(card);
+        if (result.length !== 0){
+            returnVal = false;
+        }
+    }
+    return returnVal;
 }
 
 function TestPipe(){
 
-	let card1 = new PathCard("no", "yes", "no", "yes");
-	let card2 = new PathCard("yes", "no", "no", "no");
-	let card3 = new PathCard("yes", "no", "yes", "no");
-	let card4= new PathCard("no", "no", "no", "yes");
-	field.place(card1, 0, 1);
-	field.place(card2, 0, -1);
-	field.place(card3, 1, 0);
-	field.place(card4, -1, 0);	
-	let card = new PathCard ("no","yes", "yes","no");
-	let result = field.availableSpaces(card);
-	if (result[0][0] === 1 && result[0][1] === 1){
-		console.log("true");
-	}
-	else{
-		console.log("false");
-	}
-	let card_rev = new PathCard("yes", "no", "no", "yes");
-	result = field.availableSpaces(card_rev);
-	if (result[0][0] === 1 && result[0][1] === 1){
-		console.log("true");
-	}
-	else{
-		console.log("false");
-	}
+    let testField = new Field();
+    let card1 = 31;
+    testField.place(card1, 0, -1);
+    let [a, b] = testField.canPlaceInPosition(31, 1, -1);
+    let [c, d] = testField.canPlaceInPosition(24, 1, -1);
+    return  (a  === false && b === true &&
+        c === false && d === true);
 }
 
-function TestInfinity(){
-	let card = new PathCard("yes", "yes", "yes","yes",);
-	let result = field.availableSpaces(card);
-	for (let i = 0; i<1000; i++){
-		field.place(card,result[0][0], result[0][1]);
-		result = field.availableSpaces(card);
-	}
-}
+function TestReacheable(){
 
-function TestCanPlace(){
-
-	field.grid[0][0]._right = "no";
-	field.grid[0][0]._down = "no";
-	let card1 = new PathCard("yes", "no", "no", "yes");
-	let card2 = new PathCard("no", "yes", "no", "yes");
-	field.place(card1 , -1, 0);
-	field.place(card2, -1, 1);
-	let arr = [new PathCard("no", "no", "yes", "yes"), new PathCard("yes", "no", "no", "yes"), new PathCard("yes", "yes", "no", "yes", )];
-	let result;
-	for (let i = 0; i<3;i++){
-		result = field.availableSpaces(arr[i]);
-		if (result.length == 0){
-			console.log("pipe", arr[i] ,"can not be palced");
-			if (i % 2 === 0){
-				console.log("true");
-			}
-			else{
-				console.log("false");
-			}			
-		}
-		else{
-			console.log("pipe", arr[i] ,"can be palced");
-			if (i % 2 === 1 || i == 2){
-				console.log("true");
-			}
-			else{
-				console.log("false");
-			}			
-		}
-	}
+    let testField = new Field();
+    let card = 11;
+    testField.place(card, 0, -1);
+    let card1 = 19;
+    testField.place(card1, 1, 0);
+    let card2 = 38;
+    testField.place(card2, -1, 0);
+    let card3 = 42;
+    testField.place(card3, 0, 1);
+    return (testField.reachableSpaces.length == 0);
 }
 
 function TestGap(){
 
-	field.grid[0][0]._left = "no";
-	field.grid[0][0]._down = "no";
-	field.grid[0][0]._up = "no";
-	let card = new PathCard("no","no","yes","yes");
-	field.place(card, 2,0);
-	let arr = [new PathCard("no", "no", "yes", "yes"), new PathCard("yes", "no", "no", "yes"), new PathCard("yes", "yes", "no", "yes", )];
-	console.log (gap(arr));
+    let testField = new Field();
+    testField.grid[0][0] = 0;
+    let card = 28;
+    testField.place(card, 0, - 2);
+    let testData = [28, 21, 41];
+    return (gap(testField, testData, [0, -1]));
 
-	field.grid[0][0]._left = "no";
-	field.grid[0][0]._down = "no";
-	field.grid[0][0]._up = "yes";
-	field.grid[0][0]._right = "no";
-	card = new PathCard ("yes","yes", "no", "no");
-	field.place(card, 0, 2);
-	arr = [new PathCard("yes", "yes", "no", "yes", ), new PathCard("yes", "no", "no", "yes"),new PathCard("no", "no", "yes", "yes") ];
-	console.log (gap(arr));
+    testField.grid[0][0] = 0;
+    card = 21;
+    testField.place(card, 2, 0);
+    testData = [21, 28, 41];
+    return (gap(testField, testData, [1, 0]));
 
 }
 
-function gap(arr){
+function gap(testField, arr, answer){
 
-	let result;
-	for (let i = 0; i<3;i++){
-		result = field.availableSpaces(arr[i]);
-		if (result.length == 0 && i == 0){
-				return false;
-			}
-		else{
-				if (result.length > 0 && i != 0){
-					return false;
-				}
-			}
-		}
-	return true;
+    let result;
+    let answ;
+    for (let i = 0; i<3;i++){
+        result = testField.availableSpaces(arr[i]);
+        for (let [a, b] of result){
+            if (a === answer[0] && b === answer[1] && i === 0){
+                answ = true;
+            }
+             if (a === answer[0] && b === answer[1] && i !== 0){
+                answ = false;
+            }
+        }
+    }
+    return answ;
 }
-
